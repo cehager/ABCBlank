efd3b8f67e9a7c781f3faf332725352cad5eb00e
charlie <charlie80088@gmail.com>
2024-09-05 17:18:03 -0600

    Created Account Holder Command

---
+++ Application/Features/AccountHolders/CreateAccountHolderCommand.cs
@@ -0,0 +1,35 @@
+﻿using Application.Repositories;
+using Common.Request;
+using Common.Wrapper;
+using Domain;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AccountHolders
+{
+    public class CreateAccountHolderCommand : IRequest<ResponseWrapper<int>>
+    {
+        public CreateAccountHolder CreateAccountHolder { get; set; }
+    }
+
+    public class CreateAccountHolderCommandHandler(IUnitOfWork<int> unitOfWork) 
+      : IRequestHandler<CreateAccountHolderCommand, ResponseWrapper<int>>
+    {
+        private readonly IUnitOfWork<int> _unitOfWork = unitOfWork;
+            
+        public async Task<ResponseWrapper<int>> Handle(CreateAccountHolderCommand request, CancellationToken cancellationToken)
+        {
+            var accountHolder = request.CreateAccountHolder.Adapt<AccountHolder>();
+
+            await _unitOfWork.WriteRepositoryFor<AccountHolder>().AddAsync(accountHolder);
+            await _unitOfWork.CommitAsync(cancellationToken);
+
+            return new ResponseWrapper<int>().Success(accountHolder.Id, "Account Holder created successfully.");
+        }
+    }
+}
---
+++ Common/Request/AccountHolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Request
+{
+    public record CreateAccountHolder(string FirstName, string LastName, DateTime DateOfBirth, string Email, string ContactNumber);
+
+    public record UpdateAccountHolder(int Id, string FirstName, string LastName, string Email, string ContactNumber);
+
+}
